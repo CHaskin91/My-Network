f8a7fa753d799956b1c5da3e5c91d935da6b9120
Chubbs91 <chaskin1991@outlook.com>
2022-03-27 17:42:06 -0500

    setup MVC folders. start server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+// Express
+    const express = require('express')
+    const app = express()
+    const PORT = process.env.PORT || 3001
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+
+// Mongoose
+    const mongoose = require('mongoose')
