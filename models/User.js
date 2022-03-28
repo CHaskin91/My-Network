48cc59248bd5f1c12e2451f5566d30bde2f6fb17
Chubbs91 <chaskin1991@outlook.com>
2022-03-28 16:13:27 -0500

    user schema and virtual created

--- models/User.js
+++ models/User.js
@@ -1,1 +1,46 @@
 const { Schema, model } = require('mongoose');
+
+// Create Schema for User
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+\@.+\..+/]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+)
+
+// Create a Virtual called 'friendCount' that retrieves the length of the user's 'friends' array field on query.
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+});
+
+const User = model('User', UserSchema)
+
+module.exports = User
