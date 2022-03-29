95c53439248a85c038c15026094fa43fafdf2ecd
Chubbs91 <chaskin1991@outlook.com>
2022-03-28 20:46:48 -0500

    start work on thought model

---
+++ models/Thought.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (time) => format_date(time)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+})
