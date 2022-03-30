18d1040c0481c0e3d5f953bb2a69ccfab46bb5aa
Chubbs91 <chaskin1991@outlook.com>
2022-03-30 15:20:52 -0500

    thought model finished.

--- models/Thought.js
+++ models/Thought.js
@@ -1,19 +1,66 @@
 const { Schema, model } = require('mongoose');
 
-const ThoughtSchema = new Schema({
-    thoughtText: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 280
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (time) => format_date(time)
+        }
     },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (time) => format_date(time)
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+)
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (time) => format_date(time)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
     },
-    username: {
-        type: String,
-        required: true
-    },
-})
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+)
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema)
+
+module.exports = Thought
