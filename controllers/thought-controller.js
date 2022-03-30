61c7be6c566d7ee2b084ebe9902aae7becc8564f
Chubbs91 <chaskin1991@outlook.com>
2022-03-30 16:00:39 -0500

    get thought by id method added to controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -10,5 +10,14 @@ const thoughtController = {
         .catch(err => res.status(500).json(err))
     },
 
-    
+    // GET THOUGHT BY ID
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+        .populate({ path: 'reactions', select: '-__v' })
+        .select('-__v')
+        .then(dbThoughtData => dbThoughtData ? res.json(dbThoughtData) : res.status(404).json({ message: thought404Message(params.id) }))
+        .catch(err => res.status(404).json(err))
+    },
+
+    // ADD A THOUGHT
 }
